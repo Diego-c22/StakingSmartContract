aebdd7224ef710ab6faf16927132a9e4ad94381c
Diego <Diegocortes079@gmail.com>
2023-02-26 14:32:17 -0600

    Create fixtures for testing

---
+++ test/shared/fixtures.js
@@ -0,0 +1,14 @@
+const { ethers } = require('hardhat')
+const { deployMockERC20 } = require('./mocks')
+
+const deployStakingFixture = async () => {
+  const { erc20 } = await deployMockERC20()
+  const StakingContract = await ethers.getContractFactory('Staking')
+  const stakingContract = await StakingContract.deploy(erc20.address, 2000)
+
+  return { stakingContract, erc20 }
+}
+
+module.exports = {
+  deployStakingFixture,
+}
