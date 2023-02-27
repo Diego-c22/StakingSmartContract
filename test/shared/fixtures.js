de3fe36c77bcb7e51de7d2b274a5a83fb9593da0
Diego <Diegocortes079@gmail.com>
2023-02-27 12:49:38 -0600

    Create fixture for exposed contracts

--- test/shared/fixtures.js
+++ test/shared/fixtures.js
@@ -5,8 +5,10 @@ const deployStakingFixture = async () => {
   const { erc20 } = await deployMockERC20()
   const StakingContract = await ethers.getContractFactory('Staking')
   const stakingContract = await StakingContract.deploy(erc20.address, 2000)
+  const $StakingContract = await ethers.getContractFactory('$Staking')
+  const $stakingContract = await $StakingContract.deploy(erc20.address, 2000)
 
-  return { stakingContract, erc20 }
+  return { stakingContract, $stakingContract, erc20 }
 }
 
 module.exports = {
